912e2a4cbb4c9ac29fb80c2dd1cc05bc34d2b53d
Michael FIG <michael@liveblockauctions.com>
2018-12-14 11:40:32 -0600

    Configuration-driven URL matching (like cacao Dart).

--- cacao-client.ts
+++ cacao-client.ts
@@ -1,105 +1,220 @@
+// This is a function type that allows us to replace an old Cacao fetch
+// with new guts (and not leak resources when reloading the extension).
+type FetchUpgrader = (newFetch: typeof window.fetch) => typeof window.fetch;
+interface Window {
+    __CACAO_FETCH_UPGRADE__?: FetchUpgrader;
+};
+
 (function installCacao() {
-    if ((window as any).___CACAO_INSTALLED___) {
-        return;
+    let oldFetch: typeof window.fetch;
+    if (window.__CACAO_FETCH_UPGRADE__) {
+        console.log('Cacao fetch upgrading existing Cacao window.fetch');
+        oldFetch = window.__CACAO_FETCH_UPGRADE__(cacaoFetch);
+    }
+    else {
+        console.log('Cacao fetch installing new Cacao window.fetch');
+        oldFetch = window.fetch;
+        window.fetch = cacaoFetch;
     }
 
-    console.log('Cacao fetch installing extension');
-    (window as any).___CACAO_INSTALLED___ = true;
-    window.addEventListener('beforeunload', function(event) {
+    // To upgrade this instance, we remove our listeners and
+    // install the new Fetch.
+    window.__CACAO_FETCH_UPGRADE__ = (newFetch) => {
+        cacaoUnload();
+        window.fetch = newFetch;
+        return oldFetch;
+    };
+
+    // Add our event listeners and initialize.
+    window.addEventListener('message', cacaoOnMessage);
+    window.addEventListener('beforeunload', cacaoUnload);
+    send({type: 'CACAO_FETCH_INIT'});
+
+    function send(obj: any) {
+        window.postMessage(obj, '*');
+    }
+
+    // FIXME: We need to be more aggressive in preventing resource leaks
+    // on Firefox.
+    function cacaoUnload() {
         console.log(`Cacao fetch unloading`);
-        window.postMessage({
-            type: 'CACAO_FETCH_ABORT', id: '*',
-        }, '*');
-});
+        window.removeEventListener('beforeunload', cacaoUnload);
+        window.removeEventListener('message', cacaoOnMessage);
+        send({type: 'CACAO_FETCH_ABORT', id: '*'});
+    }
+
     let nextId = 0;
+    let pathMap = {};
+    let proxyHosts: string[];
     const streamControl: {[id: number]: any} = {};
     const connectors: {[id: number]: ((ret: any) => void)[]} = {};
-    window.addEventListener('message', function(event) {
+    function cacaoOnMessage(event: MessageEvent) {
         if (event.source !== window) {
             return;
         }
 
-        if (event.data.type && event.data.type === 'CACAO_FETCH_BEGIN') {
-            const {id, status, statusText, headers} = event.data;
-            const connector = connectors[id];
-            if (connector) {
-                delete connectors[id];
-                const stream = new (ReadableStream as any)({
-                    start(controller: any) {
-                        streamControl[id] = controller;
-                    },
-                    pull(controller: any) {
-    
-                    },
-                    cancel() {
-                        console.log(`Cancelling controller ${id}`);
-                        delete streamControl[id];
-                    },
-                });
+        switch (event.data.type) {
+            case 'CACAO_BODY_CHUNK': {
+                const {id, chunk} = event.data;
+                const controller = streamControl[id];
+                if (controller) {
+                    if (chunk === undefined) {
+                        // Close the stream.
+                        console.log(`Cacao fetch closing ${id}`);
+                        delete streamControl[event.data.id];
+                        controller.close();
+                    }
+                    else {
+                        // Send on the chunk.
+                        controller.enqueue(chunk);
+                    }
+                }
+                break;
+            }
 
-                const hdr = new Headers();
-                for (const key in (headers as {[key: string]: string})) {
-                    hdr.set(key, headers[key]);
-                }
-                const respInit = {
-                    status,
-                    statusText,
-                    headers: hdr,
-                };
+            case 'CACAO_FETCH_BEGIN': {
+                const {id, status, statusText, headers} = event.data;
+                const connector = connectors[id];
+                if (connector) {
+                    delete connectors[id];
+                    const stream = new (ReadableStream as any)({
+                        start(controller: any) {
+                            streamControl[id] = controller;
+                            send({type: 'CACAO_FETCH_READ_BODY', id});
+                        },
+                        pull(controller: any) {
+                            // Not apparently needed.
+                        },
+                        cancel() {
+                            console.log(`Cancelling controller ${id}`);
+                            delete streamControl[id];
+                        },
+                    });
 
-                const resp = new Response(stream, respInit);
-                connector[0](resp);
-            }
-        }
-        if (event.data.type && event.data.type === 'CACAO_FETCH_REJECTED') {
-            const {id, reason} = event.data;
-            console.log(`Cacao fetch rejecting ${id}: ${reason}`);
-            const connector = connectors[id];
-            if (connector) {
-                delete connectors[id];
-                connector[1](reason);
-            }
-        }
-        if (event.data.type && event.data.type === 'CACAO_BODY_CHUNK') {
-            const {id, chunk} = event.data;
-            const controller = streamControl[id];
-            if (controller) {
-                if (chunk === undefined) {
-                    // Close the stream.
-                    console.log(`Cacao fetch closing ${id}`);
-                    delete streamControl[event.data.id];
-                    controller.close();
+                    const hdr = new Headers();
+                    for (const key in (headers as {[key: string]: string})) {
+                        hdr.set(key, headers[key]);
+                    }
+                    const respInit = {
+                        status,
+                        statusText,
+                        headers: hdr,
+                    };
+
+                    const resp = new Response(stream, respInit);
+                    connector[0](resp);
                 }
-                else {
-                    // Send on the chunk.
-                    controller.enqueue(chunk);
+                break;
+            }
+
+            case 'CACAO_FETCH_REJECTED': {
+                const {id, reason} = event.data;
+                console.log(`Cacao fetch rejecting ${id}: ${reason}`);
+                const connector = connectors[id];
+                if (connector) {
+                    delete connectors[id];
+                    connector[1](reason);
+                }
+                break;
+            }
+
+            case 'CACAO_FETCH_CONFIG': {
+                const {config} = event.data;
+                console.log(`Cacao fetch configuring with`, config);
+                pathMap = config.pathMap;
+                proxyHosts = config.proxyHosts;
+                break;
+            }
+        }
+    };
+
+    // Taken verbatim from cacao/lib/src/resolve.dart.
+    function buildQuery(a: string, b: string) {
+        const prefix = (a === '' && b === '') ? '' : '?';
+        const sep = (a !== '' && b !== '') ? '&' : '';
+        return prefix + a + sep + b;
+    }
+
+    // Taken verbatim from cacao/lib/src/resolve.dart.
+    function resolve(requested: URL, prepend: string, add = '') {
+        const pre = new URL(prepend);
+        let path: string;
+        if (pre.pathname.endsWith('/')) {
+            if (add.startsWith('/')) {
+                path = pre.pathname + add.substr(1);
+            }
+            else {
+                path = pre.pathname + add;
+            }
+        }
+        else if (add === '' || add.startsWith('/')) {
+            path = pre.pathname + add;
+        }
+        else {
+            path = pre.pathname + '/' + add;
+        }
+        const addQuery = buildQuery(pre.search.substr(1), requested.search.substr(1));
+        pre.pathname = path;
+        pre.search = addQuery;
+        return pre;
+    }
+
+    // Taken verbatim from cacao/lib/src/findUri.dart.
+    function findUri(requested: URL, pathMap: {[path: string]: string}) {
+        // Use the longest matching pathMap.
+        const segs = requested.pathname.substr(1).split('/').map(decodeURIComponent);
+        const trailSlash = segs.length > 0 && segs[segs.length - 1] == '';
+        for (let i = segs.length - (trailSlash ? 1 : 0); i >= 0; --i) {
+            const toMatch = '/' +
+                segs.filter((_, index) => index < i).join('/');
+            const toPrepend = pathMap[toMatch];
+            if (toPrepend !== undefined) {
+                const toAdd = segs
+                    .filter((_, index) => index >= i)
+                    .map(encodeURIComponent)
+                    .join('/');
+                const suffix = trailSlash ? '/' : '';
+                return resolve(requested, toPrepend, toAdd + suffix);
+            }
+        }
+        
+        // Do the default mapping, if there is one.
+        const defaultUrl = pathMap[''];
+        if (defaultUrl === undefined) {
+            throw Error('Cacao fetch: no default Url in pathMap');
+        }
+        const toAdd = requested.pathname === '/' ? '' : requested.pathname;
+        return resolve(requested, defaultUrl, toAdd);
+    }
+
+    function cacaoFetch(input: RequestInfo, init: RequestInit = {}): Promise<Response> {
+        // We have to carefully remove non-cloneable init parameters.
+        try {
+            const {signal, ...initRest} = init;
+            if (typeof input === 'string') {
+                const url = new URL(input);
+                if (proxyHosts && proxyHosts.indexOf(url.host) >= 0) {
+                    const id = ++nextId;
+                    console.log('Cacao fetch intercepting', id, input, init);
+
+                    const dst = findUri(url, pathMap);
+                    console.log(`Cacao fetch remap ${id} ${input} -> ${dst.href}`);
+                    if (signal) {
+                        signal.addEventListener('abort', () => {
+                            send({type: 'CACAO_FETCH_ABORT', id});
+                        });
+                    }
+                    return new Promise((resolve, reject) => {
+                        connectors[id] = [resolve, reject];
+                        send({type: 'CACAO_FETCH_REQUEST', input: dst.href, init: initRest, id});
+                    });
                 }
             }
+            console.log('Cacao fetch falling back to old Fetch API', input, init);
+            return oldFetch(input, init);
         }
-    });
-    const oldFetch = window.fetch;
-    window.fetch = function cacaoFetch(input: RequestInfo, init?: RequestInit & {cacao: boolean | 'downgrade'}) {
-        const {cacao, signal, ...initRest} = init;
-        if (cacao) {
-            const id = ++nextId;
-            console.log('Cacao fetch starting', id, input, init);
-            const down = cacao === 'downgrade' ? input.toString().replace(/^(http)s:/, '$1:') : input;
-            if (signal) {
-                signal.addEventListener('abort', () => {
-                    window.postMessage({
-                        type: 'CACAO_FETCH_ABORT', id,
-                    }, '*');
-                });
-            }
-            return new Promise((resolve, reject) => {
-                connectors[id] = [resolve, reject];
-                window.postMessage({
-                    type: 'CACAO_FETCH_REQUEST',
-                    input: down, init: initRest, id
-                }, '*');
-            });
+        catch (e) {
+            return Promise.reject(e);
         }
-        console.log('Cacao fetch falling back', input, init);
-        return oldFetch(input, init);
     }
 })();
--- cacao-server.ts
+++ cacao-server.ts
@@ -1,76 +1,190 @@
-function send(obj: any, transferable: Array<Transferable> = []) {
-    window.postMessage(obj, '*');
-}
-let abortControllers: {[id: number]: AbortController} = {};
-window.addEventListener('message', function(event) {
-    if (event.source !== window) {
-        return;
+////////////////////////////////
+// FIXME: Derive from config.
+const enabledOrigins = [
+    `https://v5.poc.ts.liveblockauctions.com/ms/publish.html`,
+    `http://localhost:8080/publish.html`,
+//    `file:///Users/michael/src/*`
+];
+
+const pathMap = {
+    '/mjpeg': 'http://216.8.159.21/mjpg/video.mjpg',
+    '/cam1': 'http://192.168.100.1/mjpeg',
+    '/cam2': 'http://192.168.100.2/-wvhttp-01-/video.cgi',
+    '/cam3': 'http://root:VB-C300@192.168.100.3/-wvhttp-01-/GetOneShot?frame_count=0',
+    '/cam4': 'http://192.168.100.4/-wvhttp-01-/video.cgi',
+};
+
+const proxyHosts = ['127.0.0.1:9000', 'localhost:9000'];
+
+const whitelistDebug = false;
+/////////////////////////////////
+
+const href = document.URL;
+const currentUrl = new URL(href);
+
+function wlDebug(...args: any[]) {
+    if (whitelistDebug) {
+        console.log(...args);
     }
-    // console.log('Cacao got', event.data);
-    if (event.data.type && event.data.type === 'CACAO_FETCH_ABORT') {
-        const {id} = event.data;
-        console.log(`Cacao fetch aborting ${id}`);
-        if (id === '*') {
-            const ab = abortControllers;
-            abortControllers = {};
-            for (const i in ab) {
-                const abortController = ab[i];
-                abortController.abort();
-            }
+}
+
+function buildRegExp(pattern: string, toAppend = '') {
+    const re = pattern.replace(/[\\\*\$\^\.\*\+]/g, (substring) => {
+        if (substring === '*') {
+            return '.*';
+        }
+        else {
+            return '\\' + substring;
+        }
+    });
+    return new RegExp(`^${re}${toAppend}\$`, 'i');
+}
+
+let doInstall = false;
+for (const urlStr of enabledOrigins) {
+    const url = new URL(urlStr);
+    if (url.protocol !== currentUrl.protocol) {
+        wlDebug(`Cacao whitelist: ${href} does not match protocol ${url.protocol}`);
+        continue;
+    }
+    const hostRe = buildRegExp(decodeURIComponent(url.host));
+    if (!currentUrl.host.match(hostRe)) {
+        wlDebug(`Cacao whitelist: ${href} does not match host ${hostRe}`);
+        continue;
+    }
+    const pathRe = buildRegExp(url.pathname, '(/.*|)');
+    if (!currentUrl.pathname.match(pathRe)) {
+        wlDebug(`Cacao whitelist: ${href} does not match path ${pathRe}`);
+        continue;
+    }
+
+    wlDebug(`Cacao whitelist: ${href} matched ${urlStr}`);
+    doInstall = true;
+    break;
+}
+
+if (!doInstall) {
+    console.log(`Cacao ignoring ${href}; not whitelisted`);
+}
+else {
+    console.log(`Cacao hooking into ${href} Fetch API`);
+    function send(obj: any, transferable: Array<Transferable> = []) {
+        window.postMessage(obj, '*');
+    }
+    let abortControllers: {[id: number]: AbortController} = {};
+    let responses: {[id: number]: Response} = {};
+    window.addEventListener('message', function(event) {
+        if (event.source !== window) {
             return;
         }
-        const abortController = abortControllers[id];
-        if (abortController) {
-            delete abortControllers[id];
-            abortController.abort();
-        }
-    }
-    if (event.data.type && event.data.type === 'CACAO_FETCH_REQUEST') {
-        const {id, init, input} = event.data;
-        // Allow aborting.
-        const abortController = new AbortController();
-        abortControllers[id] = abortController;
-        init.signal = abortController.signal;
-        fetch(input, init).then(resp => {
-            if (!resp.ok) {
-                send({type: 'CACAO_FETCH_REJECTED', id, reason: 'not ok'});
-                return;
-            }
-            const headers: {[key: string]: string} = {};
-            resp.headers.forEach((val, key) => {
-                headers[key] = val;
-            });
-            send({type: 'CACAO_FETCH_BEGIN', id,
-                status: resp.status,
-                statusText: resp.statusText,
-                headers,
-            });
 
-            const reader = resp.body.getReader();
-            function pushRead() {
-                reader.read().then(function readChunk(attrs) {
-                    if (attrs.done) {
-                        send({type: 'CACAO_BODY_CHUNK', id, chunk: undefined});
+        // console.log('Cacao got', event.data);
+        switch (event.data.type) {
+            case 'CACAO_FETCH_ABORT': {
+                const {id} = event.data;
+                console.log(`Cacao fetch aborting ${id}`);
+                if (id === '*') {
+                    const ab = abortControllers;
+                    abortControllers = {};
+                    for (const i in ab) {
+                        const abortController = ab[i];
+                        abortController.abort();
+                    }
+                    return;
+                }
+                const abortController = abortControllers[id];
+                if (abortController) {
+                    delete abortControllers[id];
+                    abortController.abort();
+                }
+                break;
+            }
+        
+            case 'CACAO_FETCH_REQUEST': {
+                const {id, init, input} = event.data;
+
+                const myInit = {...init};
+
+                // Allow aborting.
+                const abortController = new AbortController();
+                abortControllers[id] = abortController;
+                myInit.signal = abortController.signal;
+
+                // Merge headers.
+                const hdr = new Headers();
+                for (const key in (init.headers || {})) {
+                    hdr.set(key, init.headers[key]);
+                }
+
+                // Replace embedded credentials with a header.
+                const url = new URL(input);
+                if (url.username || url.password) {
+                    const creds = btoa(url.username + ':' + url.password);
+                    hdr.append('Authorization', `Basic ${creds}`);
+                    url.username = '';
+                    url.password = '';
+                }
+                myInit.headers = hdr;
+                
+                // Actually send the request.
+                fetch(url.href, myInit).then(resp => {
+                    if (!resp.ok) {
+                        send({type: 'CACAO_FETCH_REJECTED', id, reason: 'not ok'});
                         return;
                     }
-
-                    const chunk = attrs.value;
-                    send({type: 'CACAO_BODY_CHUNK', id, chunk}, [chunk]);
-                    pushRead();
-                }).catch(function onError(error) {
-                    console.error(error);
-                    send({type: 'CACAO_BODY_CHUNK', id, chunk: undefined});
+                    const headers: {[key: string]: string} = {};
+                    resp.headers.forEach((val, key) => {
+                        headers[key] = val;
+                    });
+                    responses[id] = resp;
+                    send({type: 'CACAO_FETCH_BEGIN', id,
+                        status: resp.status,
+                        statusText: resp.statusText,
+                        headers,
+                    });
                 })
+                .catch(reason => {
+                    send({type: 'CACAO_FETCH_REJECTED', id, reason: reason.toString()});
+                });
+                break;
             }
-                    
-            pushRead();
-        })
-        .catch(reason => {
-            send({type: 'CACAO_FETCH_REJECTED', id, reason: reason.toString()});
-        });
-    }
-});
 
-const scr = document.createElement("script");
-scr.src = browser.extension.getURL('cacao-client.js');
-(document.head || document.documentElement as any).appendChild(scr);
+            case 'CACAO_FETCH_READ_BODY': {
+                const {id} = event.data;
+
+                const resp = responses[id];
+                if (resp) {
+                    const reader = resp.body.getReader();
+                    function pushRead() {
+                        reader.read().then(function readChunk(attrs) {
+                            if (attrs.done) {
+                                send({type: 'CACAO_BODY_CHUNK', id, chunk: undefined});
+                                return;
+                            }
+
+                            const chunk = attrs.value;
+                            send({type: 'CACAO_BODY_CHUNK', id, chunk}, [chunk]);
+                            pushRead();
+                        }).catch(function onError(error) {
+                            console.error(error);
+                            send({type: 'CACAO_BODY_CHUNK', id, chunk: undefined});
+                        })
+                    }
+                            
+                    pushRead();
+                }
+                break;
+            }
+
+            case 'CACAO_FETCH_INIT': {
+                send({type: 'CACAO_FETCH_CONFIG',
+                    config: {pathMap, proxyHosts}});
+                break;
+            }
+        }
+    });
+
+    const scr = document.createElement("script");
+    scr.src = browser.extension.getURL('cacao-client.js');
+    (document.head || document.documentElement).appendChild(scr);
+}
